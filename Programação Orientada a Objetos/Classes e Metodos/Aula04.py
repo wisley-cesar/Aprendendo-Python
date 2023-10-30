e7e90b922299b8f55d34fbf48c08729e9e0840a0
Wisley César <wisleycesar32@gmail.com>
2023-10-30 17:41:31 -0300

    POO

---
+++ Programação Orientada a Objetos/Classes e Metodos/Aula04.py
@@ -0,0 +1,20 @@
+nome = 'wisley'
+
+
+print(type(nome))
+
+print(nome.capitalize())
+
+class tv(): # é uma classe em ptyhon
+    cor = 'preta'
+    temanho = 55
+    volume = 50
+    def mudar_canal(self): # são os métodos em pyhton
+        pass
+    def mudar_volume(self):# método de almentar volume
+        pass
+    def ligar_desligar(self): # método de ligar e desligar
+        pass
+
+tv.mudar_canal() #estamos chamando os métodos
+tv.ligar_desligar()# chamando o método de ligar e desligar a tv
