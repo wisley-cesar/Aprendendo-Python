54768dbabbcf0efb0949c0a69301e132487f3a64
Wisley César <wisleycesar32@gmail.com>
2023-10-30 16:58:21 -0300

    Python

---
+++ Aprendendo o Basico/Aprendendo o If no Python/If- condções do python/if.py
@@ -0,0 +1,10 @@
+meta = 50000
+qtd_vendas = int(input('Quantas unidades foram vendidas ??'))
+
+if qtd_vendas >= meta : 
+    print(f"Batemos a nossa meta de vendas de IPhone, vendemos {qtd_vendas} unidades e nossa meta era de {meta} unidades")
+else : 
+    print(f'Infelizmente não batemos nossa meta de {meta} unidades, vendemos só {qtd_vendas} unidadeas ')
+
+
+    
