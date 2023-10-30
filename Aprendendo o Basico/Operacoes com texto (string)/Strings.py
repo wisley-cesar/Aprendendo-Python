837d4ec33bb618a06fa4448a7e700da23a18faaa
Wisley César <wisleycesar32@gmail.com>
2023-10-30 16:38:55 -0300

    Basico Python

---
+++ Aprendendo o Basico/Operacoes com texto (string)/Strings.py
@@ -0,0 +1,14 @@
+#isso e uma string
+print("meu nome e Wisley")
+
+#Operacao de concatenacao 
+print("meu nome e " + "Wisley")
+#Operacao in ela serve para verificar se um texto esta contido em outro 
+print("meu nome e" in "nome")
+#ele vai resultar em false 
+#mas ja nesse exemplo abaixo 
+print("w" in "wisley")
+#mas se o w tivesse em maiusculo ele iria retonar false 
+# O nome disso e case sensitive
+
+print("W "in "wisley")
---
+++ Aprendendo o Basico/Pegando informacoes do usuario/Input.py
@@ -0,0 +1,9 @@
+input("qual e o seu nome?")  # o usario vai inserir o nome mas nao vai ficar armazenada pois nao tem nehuma variavel para armazenar
+
+
+nome = input("qual e o seu nome?") # nessa caso o nome inserido pelo usuario ficara armazenado na variavel nome 
+print(nome)
+
+nome = input("digite o seu primeiro nome")
+sobrenome = input("digite seu sobrenome")
+print(nome + "" +  sobrenome)
