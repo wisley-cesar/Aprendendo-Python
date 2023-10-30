d432e76fdef4ee2c7963cf9746353c96590eabe3
Wisley César <wisleycesar32@gmail.com>
2023-10-30 16:30:35 -0300

    Basico Python

---
+++ Aprendendo o Basico/Operacoes basicas/OperacoesBasicas.py
@@ -0,0 +1,12 @@
+#SOMA
+print(3 + 3)
+#SUBTRACAO 
+print(5 - 3)
+#MULTIPLICAO 
+print(5 * 2)
+#DIVISAO
+print(20 / 10)
+#POTENCIA
+print(2 ** 3)
+#RESTO DA DIVISAO
+print(11 % 2)
---
+++ Aprendendo o Basico/Operacoes basicas/UmaObeservacao.py
@@ -0,0 +1,6 @@
+#O python segue a mesma logica que a matematica convencional segue 
+#ele vai respeitar a ordem da operacoes matematica, como? Exemplo 
+
+print(2+2*3) #Ele fez primeiro a operacao matematica seguindo a ordem da matematica 
+#e se eu quisese que ele somasse primeiro, teria que colocar um parentes antes igualzinho a matematica mesmo 
+print((2+2)*3)
